d5c281e621521e3ac60d5a31b6fd0904e6dbb54e
Joshua Oppel <joshua.oppel@oppcloud.eu>
2022-08-31 13:19:55 +0200

    Version 1.0

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from holiday_generator import generate_holidays
+from holidays import HolidayBase
 
 if __name__ == "__main__":
     country = input("Land (DE/AT): ")
     state = input("Bundesland: ")
     year = input("Jahr: ")
-    for holiday in generate_holidays(country=country, state_abbr=state, year=int(year)):
-        print(holiday.__str__())
+    holidays: list[HolidayBase] = generate_holidays(country=country, state_abbr=state, year=int(year))
+    for holiday in holidays:
+        print(holiday)
